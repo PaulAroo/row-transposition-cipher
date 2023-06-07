70dff7cf279f99c2f4be345d5ade63e6cd9c9a3c
Paul_Aroo <timmypaul2006@gmail.com>
2023-06-07 23:22:21 +0100

    setup: chakra ui config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
+import chakraTheme from '@chakra-ui/theme'
+
+const { Button, Input } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+    Input,
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ChakraBaseProvider theme={theme}>
+      <App />
+    </ChakraBaseProvider>
+  </React.StrictMode>
 )
